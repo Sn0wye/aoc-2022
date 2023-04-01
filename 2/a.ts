96f57d5f466d564cf47e2f50c42a7a2aa0786044
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-04-01 14:11:30 -0300

    Day 2

---
+++ 2/a.ts
@@ -0,0 +1,34 @@
+const translate = {
+  X: 'A',
+  Y: 'B',
+  Z: 'C'
+};
+
+const input = await Deno.readTextFile('./input.txt');
+
+const lines = input.split('\n');
+
+const parsed = lines.map(line => line.split(' ') as [string, string]);
+
+let score = 0;
+
+parsed.forEach(item => {
+  let c = 0;
+
+  let [them, me] = item;
+
+  if (me === 'X') c += 1;
+  if (me === 'Y') c += 2;
+  if (me === 'Z') c += 3;
+
+  me = translate[me as 'X' | 'Y' | 'Z'];
+
+  if (me === them) c += 3;
+  if (them === 'A' && me === 'B') c += 6;
+  if (them === 'B' && me === 'C') c += 6;
+  if (them === 'C' && me === 'A') c += 6;
+
+  score += c;
+});
+
+console.log(score);
