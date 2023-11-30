38b82f355933b41b3de6b584e5b9be55c196e749
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-11-29 23:37:16 -0300

    Day 7 pt.1

---
+++ 7/a.ts
@@ -0,0 +1,74 @@
+const lines = (await Deno.readTextFile('./input.txt')).split('\n');
+
+type Dir = {
+  parent?: Dir;
+  files: { [name: string]: number };
+  dirs: { [name: string]: Dir };
+};
+
+type Command = 'cd' | 'ls';
+
+const filesystem: Dir = { parent: undefined, files: {}, dirs: {} };
+let currentDir = filesystem;
+
+const isCommand = (line: string) => line.startsWith('$');
+
+lines.forEach(line => {
+  if (isCommand(line)) {
+    const sanitized = line.slice(2);
+    const [command, dir] = sanitized.split(' ') as [Command, string];
+
+    if (command === 'cd') {
+      if (dir === '/') {
+        currentDir = filesystem;
+        return;
+      }
+
+      if (dir === '..') {
+        if (currentDir.parent) {
+          currentDir = currentDir.parent;
+        }
+        return;
+      }
+
+      if (!currentDir.dirs[dir]) {
+        currentDir.dirs[dir] = { parent: currentDir, files: {}, dirs: {} };
+      }
+      currentDir = currentDir.dirs[dir];
+    }
+    return;
+  }
+
+  // it is listing files
+  const [first, second] = line.split(' ');
+
+  if (first === 'dir') {
+    currentDir.dirs[second] = { parent: currentDir, dirs: {}, files: {} };
+  } else {
+    // its a file
+    currentDir.files[second] = parseInt(first);
+  }
+});
+
+const dirSizes: number[] = [];
+
+const computeFileSize = (dir: Dir): number => {
+  let size = 0;
+
+  for (const file in dir.files) {
+    size += dir.files[file];
+  }
+
+  for (const dirName in dir.dirs) {
+    const dirSize = computeFileSize(dir.dirs[dirName]);
+    size += dirSize;
+    dirSizes.push(dirSize);
+  }
+
+  return size;
+};
+
+computeFileSize(filesystem);
+
+const sumOf100kFiles = dirSizes.filter(s => s < 100000).reduce((a, b) => a + b);
+console.log(sumOf100kFiles);
